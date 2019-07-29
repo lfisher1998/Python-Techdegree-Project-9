0f0c5e337a2d1507f0a6ef6019c07697650bdce3
lfisher1998 <46456099+lfisher1998@users.noreply.github.com>
2019-07-29 14:03:20 -0400

    Update views.py

--- project9/menu/views.py
+++ project9/menu/views.py
@@ -7,6 +7,7 @@ from .models import *
 from .forms import *
 
 def menu_list(request):
+    """ Grab all menu objects, filter them out by expiration date, return """
     all_menus = Menu.objects.all().prefetch_related('items')
     menus = all_menus.filter(
         expiration_date__gte=timezone.now()
@@ -14,10 +15,12 @@ def menu_list(request):
     return render(request, 'menu/list_all_current_menus.html', {'menus': menus})
 
 def menu_detail(request, pk):
+    """ Pull information regarding a specific menu """
     menu = Menu.objects.get(pk=pk)
     return render(request, 'menu/menu_detail.html', {'menu': menu})
 
 def item_detail(request, pk):
+    """ Pull information regarding a specific item """
     try: 
         item = Item.objects.get(pk=pk)
     except ObjectDoesNotExist:
@@ -25,8 +28,10 @@ def item_detail(request, pk):
     return render(request, 'menu/detail_item.html', {'item': item})
 
 def create_new_menu(request):
+    """ Create a new menu object """
     if request.method == "POST":
         form = MenuForm(request.POST)
+        # Check for validation
         if form.is_valid():
             menu = form.save(commit=False)
             menu.save()
